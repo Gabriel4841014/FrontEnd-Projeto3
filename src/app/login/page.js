8c8ee87af12a31478e19d37b5ba750774f21ef9c
Monteiro <ultimacontagabi@outlook.com>
2025-06-02 15:09:01 -0300

    Adicionando sigup e login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -21,7 +21,7 @@ export default function EntrarLogin() {
                 <p className="justify-start text-[#E1D5C2] text-4xl font-['Gilda_Display'] mb-10">Sua jornada começa aqui!</p>
                 <p className="w-[456px] text-center justify-start text-[#FFFFFF] text-lg font-['Gilda_Display'] mb-10">Os melhores vinhos nacionais te esperam. Crie sua conta e tenha acesso a uma seleção única de rótulos que traduzem a riqueza e a essência das nossas vinícolas.</p>
 
-                <button className="w-64 h-10 rounded-[10px] bg-[#E1D5C2] text-[#000000]" href="#">Não tem conta? Cadastre-se já!</button>
+                <a className="w-auto h-auto rounded-[10px] pt-2 pb-2 pl-2 pr-2 bg-[#E1D5C2] text-[#000000] cursor-pointer" href="signup">Não tem conta? Cadastre-se já!</a>
             </div>
 
             {/* Entrar - Login */}
@@ -30,7 +30,7 @@ export default function EntrarLogin() {
                 <p className="justify-start text-[#EAE5E1] text-4xl font-['Gilda_Display'] mb-8">Bem vindo de volta!</p>
 
                 <div>
-                    <p class="w-16 justify-start text-[#E1D5C2] text-base font-['Gilda_Display']">E-mail</p>
+                    <p className="w-16 justify-start text-[#E1D5C2] text-base font-['Gilda_Display']">E-mail</p>
                     <input
                         type="email"
                         placeholder="exemplo@gmail.com"
@@ -38,7 +38,7 @@ export default function EntrarLogin() {
                 </div>
 
                 <div>
-                    <p class="w-16 h-3.5 justify-start text-[#E1D5C2] text-base font-['Gilda_Display'] mt-3">Senha<br /></p>
+                    <p className="w-16 h-3.5 justify-start text-[#E1D5C2] text-base font-['Gilda_Display'] mt-3">Senha<br /></p>
                     <input
                         type="password"
                         placeholder="Senha"
@@ -49,7 +49,7 @@ export default function EntrarLogin() {
                 <a href="#" className="justify-start text-stone-200 text-base font-normal font-['Gilda_Display'] mt-5 mb-5">Esqueceu sua senha?</a>
 
                 <div className="flex justify-center items-center">
-                    <button href="#" className="bg-[#20232A] text-[##FFFFFF] py-2 px-4 rounded-[5px] w-[70%] mb-2">Entrar</button>
+                    <button href="#" className="cursor-pointer bg-[#20232A] text-[##FFFFFF] py-2 px-4 rounded-[5px] w-[70%] mb-2">Entrar</button>
                 </div>
 
                 <div className="flex items-center mb-4">
@@ -59,7 +59,7 @@ export default function EntrarLogin() {
                 </div>
 
                 <div className="flex justify-center items-center">
-                    <button className="bg-[#EAE5E1] text-[#3F0D09CC] gap-3 py-2 px-4 rounded flex items-center justify-center">
+                    <button className="bg-[#EAE5E1] cursor-pointer text-[#3F0D09CC] gap-3 py-2 px-4 rounded flex items-center justify-center">
                         <Image src={Google} alt="Google Icon" width={24} height={24} className="mr-2" />
                         Continuar com o Google
                     </button>
